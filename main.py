d861775c86468cbb777598578a8a76f487130142
ЧК <katya_tch@mail.ru>
2024-04-08 23:07:58 +0500

    запуск бота для телеги (пока что без учета контекста переписки)

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from aiogram import types, F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
 import requests
 from embeddings import select_from_db, get_embedding, select_simular_question, connect_to_db
 
+router = Router()
 with open("keys/folder_id.txt", 'r') as f:
     FOLDER_ID = f.read()
-prompt = {
-    "modelUri": "gpt://" + FOLDER_ID + "/yandexgpt-lite",
-    "completionOptions": {
-        "stream": False,
-        "temperature": 0,  # 0.6 по умолчанию
-        "maxTokens": "2000"  # 2000 было по умолчанию
-    },
-    "messages": [
-        {
-            "role": "system",
-            "text": "Ты ассистент в деканате ТюмГУ и помогаешь студентам разобраться в устройстве университета. "
-                    "Отвечать на вопрос в тегах question нужно только на основе "
-                    "предоставленного документа в тегах info. Отвечай на вопросы очень кратко."
-        }
-    ]
-}
 
 url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
 with open("keys/api_key.txt", 'r') as f:
@@ -28,38 +24,50 @@ headers = {
     "Authorization": f"Api-Key {API_KEY}"
 }
 
-while True:
-    if len(prompt["messages"]) >= 3:
-        prompt["messages"][-2]["text"] = prompt["messages"][-2]["text"].split("[question]")[-1].replace("[/question]", '')
-        prompt["messages"] = [
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("Привет! Я помогу тебе разобраться в устройстве ТюмГУ! Просто отправь вопрос")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    user_input = msg.text
+    query_embedding = get_embedding(user_input, text_type="query")
+    cash = select_simular_question(query_embedding)
+    prompt = {
+        "modelUri": "gpt://" + FOLDER_ID + "/yandexgpt-lite",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0,  # 0.6 по умолчанию
+            "maxTokens": "2000"  # 2000 было по умолчанию
+        },
+        "messages": [
             {
                 "role": "system",
                 "text": "Ты ассистент в деканате ТюмГУ и помогаешь студентам разобраться в устройстве университета. "
-                        "Отвечать на вопрос в тегах question нужно ТОЛЬКО на основе "
-                        "предоставленного документа в тегах info. Отвечай на вопросы кратко и приводи аргументы или примеры из документа."
-            }, prompt["messages"][-2], prompt["messages"][-1]
+                        "Отвечать на вопрос в тегах question нужно только на основе "
+                        "предоставленного документа в тегах info. Отвечай на вопросы очень кратко."
+            }
         ]
-    # print(prompt["messages"])
-
-    user_input = input("User: ")
-    query_embedding = get_embedding(user_input, text_type="query")
-    cash = select_simular_question(query_embedding)
+    }
     if cash:
-        print("Base:", cash)
-        prompt["messages"] += [{"role": "user",
-                               "text": '[question]' + user_input + '[/question]'}]
-        prompt["messages"] += [{"role": "assistant", "text": cash}]
+        await msg.answer((cash + " (Base)").replace("**", "_"))
+        # prompt["messages"] += [{"role": "user",
+        #                         "text": '[question]' + user_input + '[/question]'}]
+        # prompt["messages"] += [{"role": "assistant", "text": cash}]
     else:
         i = 1
         result = ""
         result_final = ""
-        stop_list = ["простите", "к сожалению", "извините", "прошу прощения", "не указан", "невозможно точно", "нельзя точно"]
+        stop_list = ["простите", "к сожалению", "извините", "прошу прощения", "не указан", "невозможно точно",
+                     "нельзя точно"]
         # stop_list = []
         while i <= 3:
             s = select_from_db(query_embedding, i)
             print(s.split(".txt")[0])  # название дока, который передается вместе с вопросом
             prompt["messages"] += [{"role": "user",
-                                   "text": '[info]' + s + '[/info][question]' + user_input + '[/question]'}]
+                                    "text": '[info]' + s + '[/info][question]' + user_input + '[/question]'}]
 
             response = requests.post(url, headers=headers, json=prompt)
             result = response.json()["result"]["alternatives"][0]["message"]["text"]
@@ -81,12 +89,24 @@ while True:
                 result_final = result
                 break
 
-        print("Bot: ", result_final, "\n")
-        prompt["messages"] += [{"role": "assistant", "text": result_final}]
+        await msg.answer((result_final + " (Bot)").replace("**", "_"))
+        # prompt["messages"] += [{"role": "assistant", "text": result_final}]
         conn = connect_to_db()
         cur = conn.cursor()
         cur.execute("INSERT INTO cash (question, anwser, embedding) VALUES (%s, %s, %s);",
                     (user_input, result_final, query_embedding))
         conn.commit()
         cur.close()
+    # await msg.answer(f"Твой ID: {msg.from_user.id}")
 
+
+async def main():
+    bot = Bot(token="6937003260:AAHGuzLIbFKv8FeptGgckUdP5X7aS2JRpOE", parse_mode=ParseMode.MARKDOWN)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+logging.basicConfig(level=logging.INFO)
+asyncio.run(main())
